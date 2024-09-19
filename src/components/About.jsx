5f7e467f50402716f41403c86e376c2cc3d48ea3
Lellouche <an.lellouche@gmail.com>
2024-09-19 13:59:14 +0200

    Partie About terminée

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,6 +21,60 @@ const About = () => {
               son potentiel à améliorer les diagnostics et les soins grâce à des
               solutions technologiques innovantes et performantes.
             </p>
+            <div className="space-y-4">
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  HTML & CSS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12"></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  React JS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12"></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Node JS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-8/12"></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Angular
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12"></div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 flex justify-between text-center">
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                  3
+                </h3>
+                <p>Années d'expériences</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                  10 +
+                </h3>
+                <p>Projets terminés</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                  2 +
+                </h3>
+                <p>Clients satisfaits</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
