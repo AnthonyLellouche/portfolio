df6cbf45d070005028bbd5a33aa4815c72cb96c8
Lellouche <an.lellouche@gmail.com>
2024-10-08 13:16:41 +0200

    style(project): ajout de margin sur les boutons pour visiter le site

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,17 +3,24 @@ import useDarkMode from "../params/useDarkMode";
 
 const Projects = () => {
   const { projects } = data;
-  const darkMode = useDarkMode(); 
+  const darkMode = useDarkMode();
 
   return (
-    <div className={`${darkMode ? 'bg-black text-white' : 'bg-white text-black'} py-20`} id="project">
+    <div
+      className={`${
+        darkMode ? "bg-black text-white" : "bg-white text-black"
+      } py-20`}
+      id="project"
+    >
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Mes projets</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
             <div
               key={project.id}
-              className={`${darkMode ? 'bg-gray-800' : 'bg-gray-200'} p-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105`}
+              className={`${
+                darkMode ? "bg-gray-800" : "bg-gray-200"
+              } p-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105`}
             >
               <img
                 src={`${import.meta.env.BASE_URL}assets/${project.image}`}
@@ -22,8 +29,20 @@ const Projects = () => {
                 loading="lazy"
               />
               <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
-              <p className={`${darkMode ? 'text-gray-400' : 'text-gray-700'} mb-4`}>{project.technologies}</p>
-              <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} mb-4`}>{project.description}</p>
+              <p
+                className={`${
+                  darkMode ? "text-gray-400" : "text-gray-700"
+                } mb-4`}
+              >
+                {project.technologies}
+              </p>
+              <p
+                className={`${
+                  darkMode ? "text-gray-300" : "text-gray-600"
+                } mb-4`}
+              >
+                {project.description}
+              </p>
               <a
                 href={project.github}
                 className="inline-block bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-full"
@@ -36,7 +55,7 @@ const Projects = () => {
               {project.site && (
                 <a
                   href={project.site}
-                  className="inline-block bg-gradient-to-r from-pink-500 to-yellow-500 text-white px-4 py-2 rounded-full ml-2"
+                  className="inline-block bg-gradient-to-r from-pink-500 to-yellow-500 text-white px-4 py-2 rounded-full ml-2 lg:mt-3 lg:ml-px xl:ml-2"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label={`Visiter le site de ${project.name}`}
