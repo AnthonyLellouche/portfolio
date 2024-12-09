9ae2dde025d1b30cf047d62036a17ca9dbd2dc8b
Lellouche <an.lellouche@gmail.com>
2024-12-09 15:54:54 +0100

    chore: suppression import error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
 const Error404 = () => {
   return (
     <div className="m-0 min-h-screen grid grid-rows-[auto_1fr_auto]">
