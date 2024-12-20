fbf51d9bbd14f7bb69b7c528682a898cfbe1c9de
Lellouche <an.lellouche@gmail.com>
2024-12-20 12:28:19 +0100

    fix: modifications routes boutons mes services et mes projets qui crashait le site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,14 +40,14 @@ const Hero = () => {
         </p>
         <div className="flex flex-row mt-4 p-4 rounded-lg items-center justify-center gap-6">
           <a
-            href="/#service"
+            href="#service"
             aria-label={t("hero.viewServices")}
             className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-200 hover:scale-x-105 px-4 py-2 rounded-full"
           >
             {t("hero.myServices")}
           </a>
           <a
-            href="/#project"
+            href="#project"
             aria-label={t("hero.viewProjects")}
             className="bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-200 hover:scale-x-105 px-4 py-2 rounded-full"
           >
